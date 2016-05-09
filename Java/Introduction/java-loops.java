0187ebf792d0f2fd7b565c16cce7257de949121f
Fan Jin <fanjin1989@gmail.com>
2016-05-08 18:14:11 -0700

    add java

---
+++ Java/Introduction/java-loops.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int T = input.nextInt();
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        for (int i=0;i<T;i++) {
+            //System.out.println("a");
+            int a = input.nextInt();
+            int b = input.nextInt();
+            int N = input.nextInt();
+            for (int j = 0; j < N; j++) {
+                int sum = a;
+                for (int k = 0; k <= j; k++) {
+                    sum += Math.pow(2, k) * b;
+                }
+                System.out.print(sum + " ");
+            }
+            System.out.println();
+        }
+    }
+}
